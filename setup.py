96c87ba545123692ddec79ccfc214fb40d4b0c7d
Jonas Köhler <jonas.koehler.ks@gmail.com>
2022-11-21 12:17:39 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ribflox",
+    packages=["ribflox"],
+    package_dir={"": "code"}
+    version="0.1.0",
+    url="https://github.com/noegroup/rigid-flows.git",
+    author="Jonas Köhler",
+    author_email="jonas.koehler.ks@gmail.com",
+    description="flows for rigid bodies",
+    python_requires=">3.10",
+    # install_requires=["flox @ git+ssh://git@github.com/noegroup/flox.git"],
+)

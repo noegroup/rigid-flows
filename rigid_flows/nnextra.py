6fda7fc4c0a0b44fe0e9cfdade55087465a75328
Jonas Köhler <jonas.koehler.ks@gmail.com>
2023-01-13 17:03:42 +0100

    added spline model

---
+++ rigid_flows/nnextra.py
@@ -0,0 +1,322 @@
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+
+from flox.util import key_chain
+
+
+def zero_param(x):
+    # return x
+    if eqx.is_array(x):
+        return jnp.zeros_like(x)
+    else:
+        return x
+
+
+class PositionConditionerBlock(eqx.Module):
+
+    nodes_kq: eqx.nn.Linear
+    aux_kq: eqx.nn.Linear
+    rot_kq: eqx.nn.Linear
+    values: eqx.nn.Linear
+    num_heads: int
+
+    def __init__(
+        self,
+        node_dim: int,
+        num_aux: int,
+        num_heads: int,
+        num_channels: int,
+        *,
+        key
+    ) -> None:
+        chain = key_chain(key)
+        self.num_heads = num_heads
+        self.nodes_kq = eqx.nn.Linear(
+            node_dim, num_heads * num_channels * 2, key=next(chain)
+        )
+        self.aux_kq = eqx.nn.Linear(
+            num_aux, num_heads * num_channels * 2, key=next(chain)
+        )
+        self.rot_kq = eqx.nn.Linear(
+            4, num_heads * num_channels * 2, use_bias=False, key=next(chain)
+        )
+        self.values = eqx.nn.Linear(
+            node_dim, node_dim * num_heads * 3, key=next(chain)
+        )
+
+    def __call__(self, nodes, aux, rot):
+        seq_len = nodes.shape[0]
+
+        val = jax.vmap(self.values)(nodes).reshape(
+            seq_len, 3 * self.num_heads, -1
+        )
+
+        nodes_k, nodes_q = jnp.split(
+            jax.vmap(self.nodes_kq)(nodes).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+        aux_k, aux_q = jnp.split(
+            jax.vmap(self.aux_kq)(aux).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+        rot_k, rot_q = jnp.split(
+            jax.vmap(self.rot_kq)(rot).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+
+        k = jnp.concatenate([nodes_k, aux_k, rot_k], axis=-2)
+        q = jnp.concatenate([nodes_q, aux_q, rot_q], axis=-2)
+
+        logits = jnp.einsum("ihc, jhc -> ijh", k, q)
+        logits = logits.at[..., -self.num_heads :].set(
+            jnp.square(logits[..., -self.num_heads :])
+        )
+        att = jax.nn.softmax(logits, axis=-2)
+        out = jnp.einsum("ijh, jhd -> id", att, val)
+
+        return out
+
+
+class PosConditioner(eqx.Module):
+
+    blocks: list[tuple[PositionConditionerBlock, eqx.nn.LayerNorm]]
+    decoder: eqx.nn.Linear
+
+    def __init__(
+        self, inp, out, num_aux, num_heads, num_channels, num_blocks, *, key
+    ) -> None:
+        chain = key_chain(key)
+        self.blocks = [
+            (
+                jax.tree_map(
+                    zero_param,
+                    PositionConditionerBlock(
+                        inp, num_aux, num_heads, num_channels, key=next(chain)
+                    ),
+                ),
+                eqx.nn.LayerNorm((inp,), elementwise_affine=True),
+            )
+            for _ in range(num_blocks)
+        ]
+
+        self.decoder = jax.tree_map(
+            zero_param, eqx.nn.Linear(inp, out, use_bias=True, key=next(chain))
+        )
+
+    def __call__(self, aux, rot):
+        seq_len = aux.shape[0]
+        nodes = jnp.eye(seq_len)
+        for block, norm in self.blocks:
+            nodes = nodes + jax.vmap(norm)(block(nodes, aux, rot))
+        return jax.vmap(self.decoder)(nodes)
+
+
+class AuxiliaryConditionerBlock(eqx.Module):
+
+    nodes_kq: eqx.nn.Linear
+    pos_kq: eqx.nn.Linear
+    rot_kq: eqx.nn.Linear
+    values: eqx.nn.Linear
+    num_heads: int
+
+    def __init__(
+        self, node_dim: int, num_heads: int, num_channels: int, *, key
+    ) -> None:
+        chain = key_chain(key)
+        self.num_heads = num_heads
+        self.nodes_kq = eqx.nn.Linear(
+            node_dim, num_heads * num_channels * 2, key=next(chain)
+        )
+        self.pos_kq = eqx.nn.Linear(
+            2 * 3, num_heads * num_channels * 2, key=next(chain)
+        )
+        self.rot_kq = eqx.nn.Linear(
+            4, num_heads * num_channels * 2, use_bias=False, key=next(chain)
+        )
+        self.values = eqx.nn.Linear(
+            node_dim, node_dim * num_heads * 3, key=next(chain)
+        )
+
+    def __call__(self, nodes, pos, rot):
+        pos = jnp.concatenate(
+            [jnp.cos(2 * jnp.pi * pos), jnp.sin(2 * jnp.pi * pos)], axis=-1
+        )
+
+        seq_len = nodes.shape[0]
+
+        val = jax.vmap(self.values)(nodes).reshape(
+            seq_len, 3 * self.num_heads, -1
+        )
+
+        nodes_k, nodes_q = jnp.split(
+            jax.vmap(self.nodes_kq)(nodes).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+        pos_k, pos_q = jnp.split(
+            jax.vmap(self.pos_kq)(pos).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+
+        rot_k, rot_q = jnp.split(
+            jax.vmap(self.rot_kq)(rot).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+
+        k = jnp.concatenate([nodes_k, pos_k, rot_k], axis=-2)
+        q = jnp.concatenate([nodes_q, pos_q, rot_q], axis=-2)
+
+        logits = jnp.einsum("ihc, jhc -> ijh", k, q)
+        logits = logits.at[..., -self.num_heads :].set(
+            jnp.square(logits[..., -self.num_heads :])
+        )
+        att = jax.nn.softmax(logits, axis=-2)
+        out = jnp.einsum("ijh, jhd -> id", att, val)
+
+        return out
+
+
+class AuxConditioner(eqx.Module):
+
+    blocks: list[tuple[AuxiliaryConditionerBlock, eqx.nn.LayerNorm]]
+    decoder: eqx.nn.Linear
+
+    def __init__(
+        self, inp, out, num_heads, num_channels, num_blocks, *, key
+    ) -> None:
+        chain = key_chain(key)
+        self.blocks = [
+            (
+                jax.tree_map(
+                    zero_param,
+                    AuxiliaryConditionerBlock(
+                        inp, num_heads, num_channels, key=next(chain)
+                    ),
+                ),
+                eqx.nn.LayerNorm((inp,), elementwise_affine=True),
+            )
+            for _ in range(num_blocks)
+        ]
+        self.decoder = jax.tree_map(
+            zero_param, eqx.nn.Linear(inp, out, use_bias=True, key=next(chain))
+        )
+
+    def __call__(self, pos, rot):
+        seq_len = pos.shape[0]
+        nodes = jnp.eye(seq_len)
+        for block, norm in self.blocks:
+            nodes = nodes + jax.vmap(norm)(block(nodes, pos, rot))
+        return jax.vmap(self.decoder)(nodes)
+
+
+class RotationConditionerBlock(eqx.Module):
+
+    nodes_kq: eqx.nn.Linear
+    pos_kq: eqx.nn.Linear
+    aux_kq: eqx.nn.Linear
+    values: eqx.nn.Linear
+    num_heads: int
+
+    def __init__(
+        self,
+        node_dim: int,
+        num_aux: int,
+        num_heads: int,
+        num_channels: int,
+        *,
+        key
+    ) -> None:
+        chain = key_chain(key)
+        self.num_heads = num_heads
+        self.nodes_kq = eqx.nn.Linear(
+            node_dim, num_heads * num_channels * 2, key=next(chain)
+        )
+        self.pos_kq = eqx.nn.Linear(
+            2 * 3, num_heads * num_channels * 2, key=next(chain)
+        )
+        self.aux_kq = eqx.nn.Linear(
+            num_aux,
+            num_heads * num_channels * 2,
+            use_bias=False,
+            key=next(chain),
+        )
+        self.values = eqx.nn.Linear(
+            node_dim, node_dim * num_heads * 3, key=next(chain)
+        )
+
+    def __call__(self, nodes, pos, aux):
+        pos = jnp.concatenate(
+            [jnp.cos(2 * jnp.pi * pos), jnp.sin(2 * jnp.pi * pos)], axis=-1
+        )
+
+        seq_len = nodes.shape[0]
+
+        val = jax.vmap(self.values)(nodes).reshape(
+            seq_len, 3 * self.num_heads, -1
+        )
+
+        nodes_k, nodes_q = jnp.split(
+            jax.vmap(self.nodes_kq)(nodes).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+        pos_k, pos_q = jnp.split(
+            jax.vmap(self.pos_kq)(pos).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+
+        aux_k, aux_q = jnp.split(
+            jax.vmap(self.aux_kq)(aux).reshape(seq_len, self.num_heads, -1),
+            2,
+            -1,
+        )
+
+        k = jnp.concatenate([nodes_k, pos_k, aux_k], axis=-2)
+        q = jnp.concatenate([nodes_q, pos_q, aux_q], axis=-2)
+
+        logits = jnp.einsum("ihc, jhc -> ijh", k, q)
+
+        att = jax.nn.softmax(logits, axis=-2)
+        out = jnp.einsum("ijh, jhd -> id", att, val)
+
+        return out
+
+
+class RotConditioner(eqx.Module):
+
+    blocks: list[tuple[RotationConditionerBlock, eqx.nn.LayerNorm]]
+    decoder: eqx.nn.Linear
+
+    def __init__(
+        self, inp, out, num_aux, num_heads, num_channels, num_blocks, *, key
+    ) -> None:
+        chain = key_chain(key)
+        self.blocks = [
+            (
+                jax.tree_map(
+                    zero_param,
+                    RotationConditionerBlock(
+                        inp, num_aux, num_heads, num_channels, key=next(chain)
+                    ),
+                ),
+                eqx.nn.LayerNorm((inp,), elementwise_affine=True),
+            )
+            for _ in range(num_blocks)
+        ]
+        self.decoder = jax.tree_map(
+            zero_param, eqx.nn.Linear(inp, out, use_bias=True, key=next(chain))
+        )
+
+    def __call__(self, pos, aux):
+        seq_len = pos.shape[0]
+        nodes = jnp.eye(seq_len)
+        for block, norm in self.blocks:
+            nodes = nodes + jax.vmap(norm)(block(nodes, pos, aux))
+        return jax.vmap(self.decoder)(nodes)

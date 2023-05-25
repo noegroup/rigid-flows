2a9cd4bec0f82279d5a34606dbd3c86ac78394be
Michele Invernizzi <inve.michele@gmail.com>
2023-05-25 16:56:06 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import find_packages, setup
 
 setup(
     name="rigid_flows",
-    packages=["rigid_flows"],
     version="0.1.0",
     url="https://github.com/noegroup/rigid-flows.git",
     author="Jonas Köhler",
     author_email="jonas.koehler.ks@gmail.com",
     description="flows for rigid bodies",
     python_requires=">3.10",
+    packages=find_packages(),
+    install_requires=[
+        "flox",
+        "openmm",
+        "mlparams",
+        "tensorflow_probability",
+        "mdtraj",
+        "genice2",
+        "pymbar",
+        "tensorflow",
+        "GitPython",
+    ]
 )
